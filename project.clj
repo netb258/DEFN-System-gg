4c5892d996dac4109a92375aa046b5fd762516c3
netb258 <netb258@abv.bg>
2026-08-16 12:37:27 +0300

    Modify project.clj for plugins and dependencies
    Updated project.clj to include custom repository and modified dependencies.

--- project.clj
+++ project.clj
@@ -3,12 +3,20 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  ;; :plugins [[cider/cider-nrepl "0.49.0"]]
-  :resource-paths ["local-libs/Z80Processor-4.0.0.jar"]
+  :plugins [[cider/cider-nrepl "0.49.0"]]
+  ;; Tell Leiningen where to find our custom build of the Z80Processor.
+  :repositories [["local" {:url "file:local-libs" 
+                           :username "" 
+                           :password "" 
+                           :checksum :warn
+                           :snapshots false}]]
   :dependencies [[org.clojure/clojure "1.11.3"]
-                 [quil "2.7.1"]]
-                 ;; [com.codingrodent.microprocessor/Z80Processor "4.2.0"]
+                 [seesaw "1.4.5"]
+                 [quil "4.3.1563"]
+                 [com.esotericsoftware/kryo "5.6.0"]
+                  [org.objenesis/objenesis "3.3"]
+                 [com.codingrodent.microprocessor/Z80Processor "4.0.0-local"]]
   :main ^:skip-aot z80.core
-  :target-path "target/%s"
+  ;; :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
